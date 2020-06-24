112236a887dac779a46c27175b204a86473790a7
ppcdubai <66613116+ppcdubai@users.noreply.github.com>
2020-06-24 22:25:13 +1200

    [CHANGE] salary_per_h position in hr_contract_views.xml

--- ohrms_overtime/models/hr_contract.py
+++ ohrms_overtime/models/hr_contract.py
@@ -3,6 +3,5 @@ from odoo import models, fields
 
 class HrContractOvertime(models.Model):
     _inherit = 'hr.contract'
-    salary_per_h = fields.Float(string= "Standard Salary per Hour")
     over_hour = fields.Monetary('Overtime Hour Wage')
     over_day = fields.Monetary('Overtime Day Wage')
